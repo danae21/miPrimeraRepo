4159ccd38922f769699951e8f43a53ae679894ab
danae21 <danaedelgado21@gmail.com>
2022-08-26 12:17:38 -0400

    subiendo actividad

---
+++ prediccion_de_codigo_2.js
@@ -0,0 +1,19 @@
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+//la funcion me va a mostrar el arreglo completo 
+
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+//var frozen = [brocoli, helado, croqueta de papa]
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+//movielibrary = bambi, beetlejuice , toy story, zoro 
