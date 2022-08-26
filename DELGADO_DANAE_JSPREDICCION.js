73d16baedb640098faae792c9a18beb7a0aa96c5
danae21 <danaedelgado21@gmail.com>
2022-08-26 11:27:09 -0400

    subiendo jsprediccion

---
+++ DELGADO_DANAE_JSPREDICCION.js
@@ -0,0 +1,21 @@
+function myBirthYearFunc(){
+        console.log("Nací en " + 1980);
+    }
+    //console.log imprime: Naci en 1980
+
+    function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }
+    //console.log imprime: naci en 1980
+
+    function add(num1, num2){
+        console.log("¡Sumando números!");
+        console.log("num1 is: " + num1);
+        console.log("num2 is: " + num2);
+        var sum = num1 + num2;
+        console.log(sum);
+    }
+    //console.long imprime: ¡sumando numeros!
+    //console.long imprime: num is 10
+    //console.log imprime: num is 20
+    //console.log imprime: 30+-
